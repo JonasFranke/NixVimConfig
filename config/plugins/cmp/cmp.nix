44a8c71203f7997b401be9850dab55c423756a47
Jonas Franke <info@jonasfranke.xyz>
2026-08-22 15:59:14 +0200

    Add nvim-autopairs

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -1,4 +1,9 @@
 _: {
+  extraConfigLua = ''
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  '';
   plugins = {
     luasnip.enable = true;
     friendly-snippets.enable = true;
---
+++ config/plugins/editor/autopairs.nix
@@ -0,0 +1,9 @@
+_: {
+  plugins.nvim-autopairs = {
+    enable = true;
+    settings = {
+      check_ts = true;
+      disable_filetype = [ "TelescopePrompt" "vim" ];
+    };
+  };
+}
